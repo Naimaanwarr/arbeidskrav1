bcc75672b2002e8881e9490e9d7f0221879cde66
Naimaanwarr <naimaanwarr@gmail.com>
2024-10-28 18:58:54 +0100

    Book form

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<h1>Books I want to read</h1>)
+function BookForm({onNewBook}) {
+
+    const [title, setTitle] = React.useState("");
+
+    function handleNewAdd(e) {
+        e.preventDefault();
+        onNewBook({title})
+        setTitle("");
+    }
+
+    return <form onSubmit={handleNewAdd}>
+        <label>
+            Title:<input
+            type="text"
+            value={title}
+            onChange={(e) => {
+                setTitle(e.target.value);
+            }}
+            />
+        </label>
+        <button>Add</button>
+    </form>;
+}
+
+function LibraryApplication() {
+
+    const [books, setBooks] = useState([])
+
+    function handleNewBook(book) {
+        setBooks(prevBooks => [...prevBooks, book]);
+    }
+
+    return <>
+        <h1>Books I want to read</h1>
+
+        <ul>
+            {books.map((book) => (<li>{book.title}</li>))}
+        </ul>
+
+        <BookForm onNewBook={handleNewBook} />
+
+        </>;
+}
+
+root.render(<LibraryApplication  />)
