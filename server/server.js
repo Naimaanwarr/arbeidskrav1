e009f8241168e597cebc23b448ce2991cc7c6428
Naimaanwarr <naimaanwarr@gmail.com>
2024-10-28 20:09:10 +0100

    /api/books

--- server/server.js
+++ server/server.js
@@ -2,4 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/books", (req, res) => {
+    res.send([{
+        title: "the book form the server"
+    }])
+})
+
+
+
+
 app.listen(3000);
