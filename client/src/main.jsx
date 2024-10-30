be74680f76e06e06e1cd74806a7910e046208e14
Naimaanwarr <naimaanwarr@gmail.com>
2024-10-30 19:35:09 +0100

    npm install prettier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,79 +1,76 @@
-import React, {useEffect, useState} from 'react';
-import {createRoot} from "react-dom/client";
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-function BookForm({onNewBook}) {
+function BookForm({ onNewBook }) {
+  const [title, setTitle] = React.useState("");
 
-    const [title, setTitle] = React.useState("");
+  function handleNewAdd(e) {
+    e.preventDefault();
+    const book = { title };
+    onNewBook({ title });
+    setTitle("");
+  }
 
-    function handleNewAdd(e) {
-        e.preventDefault();
-        const book ={title};
-        onNewBook({title});
-        setTitle("");
-    }
-
-    return <form onSubmit={handleNewAdd}>
-        <label>
-            Title:<input
-            type="text"
-            value={title}
-            onChange={(e) => {
-                setTitle(e.target.value);
-            }}
-            />
-        </label>
-        <button>Add</button>
-    </form>;
+  return (
+    <form onSubmit={handleNewAdd}>
+      <label>
+        Title:
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </label>
+      <button>Add</button>
+    </form>
+  );
 }
 
 function LibraryApplication() {
+  const [books, setBooks] = useState([]);
 
-    const [books, setBooks] = useState([])
-
-
-    async function loadLibrary(){
-        const res = await fetch("/api/books");
-        if (res.ok){
-           setBooks( await res.json());
-        }
-        else{
-            console.log("error occured");
-        }
+  async function loadLibrary() {
+    const res = await fetch("/api/books");
+    if (res.ok) {
+      setBooks(await res.json());
+    } else {
+      console.log("error occured");
     }
+  }
 
-    useEffect(() => {
-        loadLibrary();
-    }, [])
+  useEffect(() => {
+    loadLibrary();
+  }, []);
 
-    async function handleNewBook(book) {
-        await fetch("/api/books", {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(book),
-        })
-        await loadLibrary();
+  async function handleNewBook(book) {
+    await fetch("/api/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
+    });
+    await loadLibrary();
+  }
 
-    }
+  return (
+    <>
+      <h1>Books I want to read</h1>
 
+      {books.map((book, index) => (
+        <div key={index}>
+          <input type="checkbox" />
+          {book.title}
+        </div>
+      ))}
 
-
-    return <>
-        <h1>Books I want to read</h1>
-
-            {books.map((book, index) => (
-                <div key={index}>
-                    <input type="checkbox"/>
-                    {book.title}
-                </div>
-            ))}
-
-        <BookForm onNewBook={handleNewBook}/>
-
-    </>;
+      <BookForm onNewBook={handleNewBook} />
+    </>
+  );
 }
 
-root.render(<LibraryApplication/>)
+root.render(<LibraryApplication />);
--- server/server.js
+++ server/server.js
@@ -3,22 +3,23 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const tasks = [{
-    title: "the book from the server"
-}];
+const tasks = [
+  {
+    title: "the book from the server",
+  },
+];
 
 app.get("/api/books", (req, res) => {
-    res.send(tasks)
-})
+  res.send(tasks);
+});
 
 app.post("/api/books", (req, res) => {
-    const {title} = req.body;
-    const book = {title}
-    tasks.push(book);
-    res.send(200);
-})
+  const { title } = req.body;
+  const book = { title };
+  tasks.push(book);
+  res.send(200);
+});
 
 app.use(express.static("../client/dist"));
 
 app.listen(process.env.PORT || 3000);
-
